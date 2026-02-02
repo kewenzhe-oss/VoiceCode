d4aa0ac3d170ca2ffc24182a0568509e58af2080
James <kewenzhe128@gmail.com>
2026-02-02 00:14:45 -0500

    Initial Commit

---
+++ VoiceCodeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceCodeApp
+//
+//  Created by Granger Brebda D. on 2/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceCodeApp/VoiceCodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceCodeAppApp.swift
+//  VoiceCodeApp
+//
+//  Created by Granger Brebda D. on 2/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct VoiceCodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
